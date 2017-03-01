87fa348a47116cfdabd6156c79e3c344b3b77436
Sakda Chutchawan <oddhome@gmail.com>
2017-03-01 09:59:39 +0700

    Add master file

---
+++ Rama9-CardReader/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Rama9_CardReader
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_read = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxReaderList = new System.Windows.Forms.ComboBox();
+            this.btnRefreshReaderList = new System.Windows.Forms.Button();
+            this.lbLibVersion = new System.Windows.Forms.Label();
+            this.PhotoProgressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tb_fullname = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lb_cid = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_read
+            // 
+            this.btn_read.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_read.Font = new System.Drawing.Font("Circular", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btn_read.Location = new System.Drawing.Point(826, 12);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(199, 143);
+            this.btn_read.TabIndex = 0;
+            this.btn_read.Text = "READ";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Circular", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 43);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ชื่อ - นามสกุล :";
+            // 
+            // cbxReaderList
+            // 
+            this.cbxReaderList.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxReaderList.FormattingEnabled = true;
+            this.cbxReaderList.Location = new System.Drawing.Point(826, 161);
+            this.cbxReaderList.Name = "cbxReaderList";
+            this.cbxReaderList.Size = new System.Drawing.Size(199, 21);
+            this.cbxReaderList.TabIndex = 3;
+            // 
+            // btnRefreshReaderList
+            // 
+            this.btnRefreshReaderList.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRefreshReaderList.Location = new System.Drawing.Point(826, 188);
+            this.btnRefreshReaderList.Name = "btnRefreshReaderList";
+            this.btnRefreshReaderList.Size = new System.Drawing.Size(199, 23);
+            this.btnRefreshReaderList.TabIndex = 4;
+            this.btnRefreshReaderList.Text = "Refresh Reader";
+            this.btnRefreshReaderList.UseVisualStyleBackColor = true;
+            this.btnRefreshReaderList.Click += new System.EventHandler(this.btnRefreshReaderList_Click);
+            // 
+            // lbLibVersion
+            // 
+            this.lbLibVersion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbLibVersion.AutoSize = true;
+            this.lbLibVersion.Location = new System.Drawing.Point(12, 425);
+            this.lbLibVersion.Name = "lbLibVersion";
+            this.lbLibVersion.Size = new System.Drawing.Size(64, 13);
+            this.lbLibVersion.TabIndex = 5;
+            this.lbLibVersion.Text = "lbLibVersion";
+            // 
+            // PhotoProgressBar1
+            // 
+            this.PhotoProgressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PhotoProgressBar1.Location = new System.Drawing.Point(826, 217);
+            this.PhotoProgressBar1.Name = "PhotoProgressBar1";
+            this.PhotoProgressBar1.Size = new System.Drawing.Size(199, 19);
+            this.PhotoProgressBar1.TabIndex = 6;
+            // 
+            // tb_fullname
+            // 
+            this.tb_fullname.Font = new System.Drawing.Font("Circular", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.tb_fullname.Location = new System.Drawing.Point(212, 12);
+            this.tb_fullname.Name = "tb_fullname";
+            this.tb_fullname.Size = new System.Drawing.Size(423, 48);
+            this.tb_fullname.TabIndex = 7;
+            this.tb_fullname.TextChanged += new System.EventHandler(this.tb_fullname_TextChanged);
+            // 
+            // btn_send
+            // 
+            this.btn_send.AutoSize = true;
+            this.btn_send.Enabled = false;
+            this.btn_send.Font = new System.Drawing.Font("Circular", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btn_send.Location = new System.Drawing.Point(641, 12);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(120, 50);
+            this.btn_send.TabIndex = 8;
+            this.btn_send.Text = "Send";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(826, 242);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(199, 193);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // lb_cid
+            // 
+            this.lb_cid.AutoSize = true;
+            this.lb_cid.Location = new System.Drawing.Point(17, 55);
+            this.lb_cid.Name = "lb_cid";
+            this.lb_cid.Size = new System.Drawing.Size(35, 13);
+            this.lb_cid.TabIndex = 10;
+            this.lb_cid.Text = "label2";
+            this.lb_cid.Visible = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 71);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(808, 351);
+            this.webBrowser1.TabIndex = 11;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 447);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.lb_cid);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.tb_fullname);
+            this.Controls.Add(this.PhotoProgressBar1);
+            this.Controls.Add(this.lbLibVersion);
+            this.Controls.Add(this.btnRefreshReaderList);
+            this.Controls.Add(this.cbxReaderList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_read);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "ถวายความอาลัย ด้วยบัตรประชาชน";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxReaderList;
+        private System.Windows.Forms.Button btnRefreshReaderList;
+        private System.Windows.Forms.Label lbLibVersion;
+        private System.Windows.Forms.ProgressBar PhotoProgressBar1;
+        private System.Windows.Forms.TextBox tb_fullname;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lb_cid;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Rama9-CardReader/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ThaiNationalIDCard;
+
+
+
+namespace Rama9_CardReader
+{
+    public partial class frmMain : Form
+    {
+        private ThaiIDCard idcard;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            idcard = new ThaiIDCard();
+            lbLibVersion.Text = "LibThaiIDCard version: " + idcard.Version();
+
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 2000; // in miliseconds
+            timer1.Start();
+
+            timer2 = new System.Windows.Forms.Timer();
+            timer2.Tick += new EventHandler(timer2_Tick);
+            timer2.Interval = 10000; // in miliseconds
+            timer2.Start();
+
+            
+
+            clearDefault();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            /*webBrowser1.Navigate(new Uri("https://stscholar.nstda.or.th/HMKingRama9/km9-list-page.php"));*/
+            /*webBrowser1.Refresh();*/
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            refreshSendButton();
+        }
+
+        public void clearDefault()
+        {
+            tb_fullname.Text = "";
+            lb_cid.Text = "";
+            btnRefreshReaderList.PerformClick();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tb_fullname.Text = "Reading....";
+                idcard.eventPhotoProgress += new handlePhotoProgress(photoProgress);
+                Personal personal = idcard.readAllPhoto();
+                
+                if (personal == null)
+                {
+                    tb_fullname.Text = "Please Insert Thai National Personal ID SmartCard";
+                    return;
+                }
+
+                pictureBox1.Image = personal.PhotoBitmap;
+                string picture_path = Environment.CurrentDirectory + "\\" + personal.Citizenid + ".jpg";
+                String picture_name = personal.Citizenid + ".jpg";
+
+                var bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.DrawToBitmap(bitmap, pictureBox1.ClientRectangle);
+                bitmap.Save(picture_name, ImageFormat.Jpeg);
+
+                lb_cid.Text = personal.Citizenid;
+                tb_fullname.Text = personal.Th_Prefix + personal.Th_Firstname + " " + personal.Th_Lastname;
+                AddDatabase(personal);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnRefreshReaderList_Click(object sender, EventArgs e)
+        {
+            cbxReaderList.Items.Clear();
+            cbxReaderList.SelectedIndex = -1;
+            cbxReaderList.SelectedText = String.Empty;
+            cbxReaderList.Text = string.Empty;
+            cbxReaderList.Refresh();
+
+            try
+            {
+                ThaiIDCard idcard = new ThaiIDCard();
+                string[] readers = idcard.GetReaders();
+                if (readers == null) return;
+
+                foreach (string r in readers)
+                {
+                    cbxReaderList.Items.Add(r);
+                }
+                cbxReaderList.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void refreshSendButton()
+        {
+            if (tb_fullname.TextLength > 0)
+            {
+                if (PingHost("www.nnr.nstda.or.th", 80) == true)
+                {
+                    btn_send.Enabled = true;
+                }
+                else
+                {
+                    btn_send.Enabled = false;
+                }
+            }
+            else
+            {
+                btn_send.Enabled = false;
+            }
+        }
+
+        public static bool PingHost(string _HostURI, int _PortNumber)
+        {
+            try
+            {
+                TcpClient client = new TcpClient(_HostURI, _PortNumber);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Error pinging host:'" + _HostURI + ":" + _PortNumber.ToString() + "'");
+                return false;
+            }
+        }
+
+        public static class dbTT
+        {
+            public static string strAccessConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=scard_local.mdb";
+        }
+
+        public void AddDatabase(Personal personal)
+        {
+            //Read Card to DB
+            //Connect to DB
+            //if (comboBox1.Text == "") return;
+
+            OleDbConnection myAccessConn = null;
+            try
+            {
+                myAccessConn = new OleDbConnection(dbTT.strAccessConn);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: Failed to create a database connection. \n{0}", ex.Message);
+                return;
+            }
+
+            OleDbCommand sql = new OleDbCommand();
+            sql.CommandType = CommandType.Text;
+            sql.CommandText = "INSERT INTO tbl_namecard_scard ([pid],[birthday],[sex],[th_title],[th_fname],[th_lname],[en_title],[en_fname],[en_lname],[issue],[expire],[address],[address_number],[address_moo],[address_lane],[address_road],[address_tambon],[address_amphur],[address_province]) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
+            sql.Parameters.AddWithValue("@pid", personal.Citizenid);
+            sql.Parameters.AddWithValue("@birthday", personal.Birthday.ToString("yyyy-MM-dd"));
+            sql.Parameters.AddWithValue("@sex", personal.Sex);
+            sql.Parameters.AddWithValue("@th_title", personal.Th_Prefix);
+            sql.Parameters.AddWithValue("@th_fname", personal.Th_Firstname);
+            sql.Parameters.AddWithValue("@th_lname", personal.Th_Lastname);
+            sql.Parameters.AddWithValue("@en_title", personal.En_Prefix);
+            sql.Parameters.AddWithValue("@en_fname", personal.En_Firstname);
+            sql.Parameters.AddWithValue("@en_lname", personal.En_Lastname);
+            sql.Parameters.AddWithValue("@issue", personal.Issue.ToString("yyyy-MM-dd"));
+            sql.Parameters.AddWithValue("@expire", personal.Expire.ToString("yyyy-MM-dd"));
+            sql.Parameters.AddWithValue("@address", personal.Address);
+            sql.Parameters.AddWithValue("@address_number", personal.addrHouseNo);
+            sql.Parameters.AddWithValue("@address_moo", personal.addrVillageNo);
+            sql.Parameters.AddWithValue("@address_lane", personal.addrLane);
+            sql.Parameters.AddWithValue("@address_road", personal.addrRoad);
+            sql.Parameters.AddWithValue("@address_tambon", personal.addrTambol);
+            sql.Parameters.AddWithValue("@address_amphur", personal.addrAmphur);
+            sql.Parameters.AddWithValue("@address_province", personal.addrProvince);
+
+            sql.Connection = myAccessConn;
+            myAccessConn.Open();
+            sql.ExecuteNonQuery();
+            myAccessConn.Close();
+
+        }
+
+        private void photoProgress(int value, int maximum)
+        {
+            if (PhotoProgressBar1.Maximum != maximum)
+                PhotoProgressBar1.Maximum = maximum;
+
+            // fix progress bar sync.
+            if (PhotoProgressBar1.Maximum > value)
+                PhotoProgressBar1.Value = value + 1;
+
+            PhotoProgressBar1.Value = value;
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            btn_send.Enabled = false;
+            String url = "https://stscholar.nstda.or.th/HMKingRama9/submit_preview.php";
+            String Response;
+
+            Response = postHtml(url, "flname", tb_fullname.Text.ToString());
+            Response = Response.Replace("src=\"","src = \"https://stscholar.nstda.or.th/HMKingRama9/");
+            //Response = Response.Replace("src: url('", "src: url('https://stscholar.nstda.or.th/HMKingRama9/");
+            Response = Response.Replace("img.src = \"", "img.src = \"https://stscholar.nstda.or.th/HMKingRama9/");
+            //System.IO.File.Delete(lb_cid.Text + ".html");
+
+            String htmlFileName = "";
+            if (lb_cid.Text == "")
+            {
+                
+                DateTime date1 = DateTime.Now;
+                htmlFileName = date1.ToString("yyyyMMddHHmmss") + ".html";
+            }
+            else
+            {
+                htmlFileName = lb_cid.Text + ".html";
+            }
+
+            System.IO.File.WriteAllText(htmlFileName, Response);
+            /*
+            string curDir = Directory.GetCurrentDirectory();
+            webBrowser1.Url = new Uri(String.Format("file:///{0}/" + htmlFileName,curDir));
+            */
+            webBrowser1.Navigate(new Uri("https://stscholar.nstda.or.th/HMKingRama9/km9-list-page.php"));
+            /*webBrowser1.Refresh();*/
+
+
+            btn_send.Enabled = true;
+        }
+
+        private void tb_fullname_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private String postHtml(string url, string field_name, string field_value)
+        {
+            NameValueCollection data = new NameValueCollection();
+            data[field_name] = field_value;
+            data["lang"] = "th";
+
+            //data.Add("field_name", field_value);
+            WebClient webClient = new WebClient();
+            try
+            {
+                byte[] responseBytes = webClient.UploadValues(url, data);
+                string response = Encoding.UTF8.GetString(responseBytes);
+                return response;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
